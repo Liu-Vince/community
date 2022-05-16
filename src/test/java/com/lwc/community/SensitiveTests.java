f71550fa443960cc04a49fbfb39bd63d4f2e6933
Liu-Vince <wenchang.liu.chn@gmail.com>
2022-05-16 21:49:14 +0800

    增加敏感词过滤功能

---
+++ src/test/java/com/lwc/community/SensitiveTests.java
@@ -0,0 +1,29 @@
+package com.lwc.community;
+
+import com.lwc.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 刘文长
+ * @version 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里不可以赌*博，嫖娼，吸毒!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
